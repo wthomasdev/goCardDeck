411ed507c90b2eca1747555bb94adb236f4c12e5
Will Thomas <wlthomas001@gmail.com>
2017-09-16 21:41:54 -0600

    add testing

---
+++ deck_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[len(d) -1] != "Four of Clubs" {
+		t.Errorf("Expected 'Four of Clubs' but got %v", d[len(d) -1])
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected 'Ace of Spades' but got %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+		os.Remove("_decktesting")
+	}
+}
